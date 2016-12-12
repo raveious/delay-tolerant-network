a639dbeeddad933abb7a9589f286fd847f29108c
Ian Wakely <raveious.irw@gmail.com>
2016-12-12 18:29:39 -0500

    Commiting app config and helper header.

--- Spray_and_Wait/SaWAppC.nc
+++ Spray_and_Wait/SaWAppC.nc
@@ -1,3 +1,5 @@
+#include "saw.h"
+
 configuration SaWAppC {
 }
 implementation {
@@ -7,12 +9,17 @@ implementation {
 	components LedsC;
 	components ActiveMessageC;
 	components new TimerMilliC();
+	components new AMSenderC(AM_SPRAYNWAIT);
+  components new AMReceiverC(AM_SPRAYNWAIT);
 
 	SaWC.Boot -> MainC.Boot;
 	SaWC.BroadcastTimer -> TimerMilliC;
 	SaWC.AMControl -> ActiveMessageC;
-	
+	SaWC.Leds -> LedsC;
+	SaWC.AMSend -> AMSenderC;
+	SaWC.Receive -> AMReceiverC;
 
-	AppC.AMSend -> SaWC.AMSendQueue;
+	AppC.AMSend -> SaWC.AppSendQueue;
 	AppC.Leds -> LedsC;
+	AppC.Receive -> SaWC.AppReceive;
 }
--- Spray_and_Wait/saw.h
+++ Spray_and_Wait/saw.h
@@ -3,6 +3,9 @@
 
 #define SPRAY_WIDTH 2
 #define BROADCAST_INTERVAL 500 // in milli-seconds
+#define MAX_PACKET_BUFFER 2
+
+#define AM_SPRAYNWAIT 6
 
 #define BROADCAST_STATE   0x01
 #define WILLING_STATE     0x02
@@ -12,9 +15,14 @@
 // TODO update this to be more like a header than a whole packet
 typedef nx_struct SprayAndWaitMsg {
 	nx_uint32_t uuid;
-	nx_uint16_t nodeID;
-	nx_uint16_t value;
-	nx_uint8_t status;
-} SprayAndWaitMsg;
+	nx_uint16_t srcNodeID;
+	nx_uint16_t destNodeID;
+	nx_uint8_t rout_stat;
+} SprayAndWaitMsg_t;
+
+typedef struct SaWQueueItem {
+	SprayAndWaitMsg_t message;
+	uint8_t available;
+} SaWQueueItem_t;
 
 #endif
